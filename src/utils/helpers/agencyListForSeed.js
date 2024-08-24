3f6d1bba81cf044a53474335861d9fb99008e03a
limon <sabur.islam@sec.ac.bd>
2024-08-24 10:16:39 +0600

    added agency model and seeded

---
+++ src/utils/helpers/agencyListForSeed.js
@@ -0,0 +1,20 @@
+const agencies = [
+    { agencyName: "Green Line Paribahan", agencyEmail: "greenline@example.com", agencyTradeNo: "GL-123456", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Shyamoli Paribahan", agencyEmail: "shyamoli@example.com", agencyTradeNo: "SH-789012", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Hanif Paribahan", agencyEmail: "hanif@example.com", agencyTradeNo: "HN-345678", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Unique Paribahan", agencyEmail: "unique@example.com", agencyTradeNo: "UN-901234", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Emon Paribahan", agencyEmail: "emon@example.com", agencyTradeNo: "EM-567890", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Sajib Paribahan", agencyEmail: "sajib@example.com", agencyTradeNo: "SJ-123456", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Sonar Bangla Paribahan", agencyEmail: "sonarbangla@example.com", agencyTradeNo: "SB-789012", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Tawakkal Paribahan", agencyEmail: "tawakkal@example.com", agencyTradeNo: "TW-345678", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Saudia Paribahan", agencyEmail: "saudia@example.com", agencyTradeNo: "SA-901234", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Starline Paribahan", agencyEmail: "starline@example.com", agencyTradeNo: "ST-567890", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Desh Travels", agencyEmail: "desh@example.com", agencyTradeNo: "DT-123456", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Barisal Express", agencyEmail: "barisal@example.com", agencyTradeNo: "BE-789012", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Sylhet Paribahan", agencyEmail: "sylhet@example.com", agencyTradeNo: "SP-345678", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Rajshahi Express", agencyEmail: "rajshahi@example.com", agencyTradeNo: "RE-901234", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Rangpur Paribahan", agencyEmail: "rangpur@example.com", agencyTradeNo: "RP-567890", createdAt: new Date(), updatedAt: new Date() },
+    { agencyName: "Limon Paribahan", agencyEmail: "sabur.islam@sec.ac.bd", agencyTradeNo: "LP-999999", createdAt: new Date(), updatedAt: new Date() }
+  ];
+
+module.exports = agencies;
